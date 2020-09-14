ada8d8aa8112403efea29b2abde4b2b418c7a280
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-14 15:40:14 +0000

    Created a program that detects if you are old enough to vote or drive

--- Main.java
+++ Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The program tells the user if they can get their drivers license
+ * @Author Thomas Quigley 
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+ 
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // get the users age
+    System.out.println("Please enter your age:");
+    int age = input.nextInt();
+
+    // initilaze the ages for driving and voting
+    final int LICENSE_AGE = 16;
+    final int VOTING_AGE = 18;
+
+    // check if they are old enough to vote or drive
+    if (age >= VOTING_AGE){
+      System.out.println("You are old enough to drive and vote!");
+    } else if (age >= LICENSE_AGE) {
+      System.out.println("You can get your driver's license!");
+    } else {
+      System.out.println("Sorry, you are not old enough to drive or vote.");
+    }
+
     
   }
 }
